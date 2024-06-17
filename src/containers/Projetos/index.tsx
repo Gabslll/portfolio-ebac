ee08c5fed85baefe92f4df070666f0d9a7f09cf5
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-06-17 16:00:09 -0300

    Creating project session and component project

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,37 @@
+import Project from '../../components/Project'
 import Title from '../../components/Title'
+
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Title fontSize={16}>Projetos</Title>
+    <Lista>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+      <li>
+        <Project></Project>
+      </li>
+    </Lista>
   </section>
 )
 
